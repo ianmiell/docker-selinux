5f2b6ff7b3004438929d849b4492a6d707f2b3dd
Ian Miell <ian.miell@gmail.com>
2015-04-24 06:49:07 +0100

    docker_selinux

--- docker_selinux.py
+++ docker_selinux.py
@@ -18,8 +18,7 @@ class docker_selinux(ShutItModule):
 		shutit.send('cd /root')
 		shutit.send('mkdir /root/selinux')
 		shutit.send('cd /root/selinux')
-		shutit.send_file('/root/selinux/docker_apache.te','''
-policy_module(docker_apache,1.0)
+		shutit.add_line_to_file('''policy_module(docker_apache,1.0)
 virt_sandbox_domain_template(docker_apache)
 allow docker_apache_t self: capability { chown dac_override kill setgid setuid net_bind_service sys_chroot sys_nice sys_tty_config } ;
 allow docker_apache_t self:tcp_socket create_stream_socket_perms;
@@ -30,12 +29,11 @@ corenet_udp_bind_all_nodes(docker_apache_t)
 corenet_udp_bind_http_port(docker_apache_t)
 sysnet_dns_name_resolve(docker_apache_t)
 permissive docker_apache_t;
-''')
-		shutit.send_file('/root/selinux/script.sh','''
-make -f /usr/share/selinux/devel/Makefile docker_apache.pp
+'''.split(),'/root/selinux/docker_apache.te')
+		shutit.add_line_to_file('''make -f /usr/share/selinux/devel/Makefile docker_apache.pp
 semodule -i docker_apache.pp
 docker run -d --security-opt type:docker_apache_t httpd
-''')
+'''.split(),'/root/selinux/script.sh')
 		shutit.send('wget -qO- https://get.docker.com/builds/Linux/x86_64/docker-latest > docker')
 		shutit.send('mv docker /usr/bin/docker')
 		shutit.send('chmod +x /usr/bin/docker')

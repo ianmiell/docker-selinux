6159222cdab1343838a5a8f831afd9c9dd7780ad
Ian Miell <ian.miell@gmail.com>
2015-05-31 14:06:19 +0100

    dupes

--- docker_selinux.py
+++ docker_selinux.py
@@ -58,24 +58,6 @@ class docker_selinux(ShutItModule):
 		shutit.send('rm -rf /root/selinux')
 		shutit.send('mkdir -p /root/selinux')
 		shutit.send('cd /root/selinux')
-		# Ensure we've cleaned up the files we're adding here.
-		shutit.add_line_to_file('''policy_module(docker_apache,1.0)
-virt_sandbox_domain_template(docker_apache)
-allow docker_apache_t self: capability { chown dac_override kill setgid setuid net_bind_service sys_chroot sys_nice sys_tty_config } ;
-allow docker_apache_t self:tcp_socket create_stream_socket_perms;
-allow docker_apache_t self:udp_socket create_socket_perms;
-corenet_tcp_bind_all_nodes(docker_apache_t)
-corenet_tcp_bind_http_port(docker_apache_t)
-corenet_udp_bind_all_nodes(docker_apache_t)
-corenet_udp_bind_http_port(docker_apache_t)
-sysnet_dns_name_resolve(docker_apache_t)
-permissive docker_apache_t;
-'''.split('\n'),'/root/selinux/docker_apache.te')
-		shutit.add_line_to_file('''make -f /usr/share/selinux/devel/Makefile docker_apache.pp
-semodule -i docker_apache.pp
-docker run -d --name selinuxdock --security-opt label:type:docker_apache_t httpd
-'''.split('\n'),'/root/selinux/script.sh')
-		shutit.send('chmod +x /root/selinux/script.sh')
 		# Ensure we have the latest version of docker.
 		shutit.send('wget -qO- https://get.docker.com/builds/Linux/x86_64/docker-latest > docker')
 		shutit.send('mv -f docker /usr/bin/docker')
@@ -118,8 +100,6 @@ corenet_udp_bind_all_nodes(docker_apache_t)
 corenet_udp_bind_http_port(docker_apache_t)
 sysnet_dns_name_resolve(docker_apache_t)
 '''.split('\n'),'/root/selinux/docker_apache.te')
-			if setenforce: 
-				shutit.add_line_to_file('permissive docker_apache_t','/root/selinux/docker_apache.te')
 			shutit.add_line_to_file('''make -f /usr/share/selinux/devel/Makefile docker_apache.pp
 semodule -i docker_apache.pp
 docker run -d --name selinuxdock --security-opt label:type:docker_apache_t httpd

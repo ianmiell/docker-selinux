deb5ad4529f892a8d8aecb45818c2ea05f901bc0
Ian Miell <ian.miell@gmail.com>
2015-05-31 13:08:24 +0100

    compile policy

--- docker_selinux.py
+++ docker_selinux.py
@@ -34,6 +34,7 @@ class docker_selinux(ShutItModule):
 		# shutit.add_line_to_file(line, filename) - add line (or lines in an array) to the filename
 		vagrant_dir = shutit.cfg[self.module_id]['vagrant_dir']
 		setenforce  = shutit.cfg[self.module_id]['setenforce']
+		compile_policy  = shutit.cfg[self.module_id]['compile_policy']
 		shutit.install('virtualbox')
 		shutit.send('wget -qO- https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb > /tmp/vagrant.deb')
 		shutit.send('dpkg -i /tmp/vagrant.deb')
@@ -49,40 +50,14 @@ class docker_selinux(ShutItModule):
 		# It should be up now, ssh into it and get root.
 		shutit.login(command='vagrant ssh')
 		shutit.login(command='sudo su')
-		# Insure required software's installed.
-		shutit.send('yum install -y wget selinux-policy-devel')
-		shutit.send('mkdir -p /root/selinux')
-		shutit.send('cd /root/selinux')
-		# Ensure we've cleaned up the files we're adding here.
-		shutit.send('rm -rf /root/selinux/docker_apache.tc /root/selinux/script.sh')
-		shutit.add_line_to_file('''policy_module(docker_apache,1.0)
-virt_sandbox_domain_template(docker_apache)
-allow docker_apache_t self: capability { chown dac_override kill setgid setuid net_bind_service sys_chroot sys_nice sys_tty_config } ;
-allow docker_apache_t self:tcp_socket create_stream_socket_perms;
-allow docker_apache_t self:udp_socket create_socket_perms;
-corenet_tcp_bind_all_nodes(docker_apache_t)
-corenet_tcp_bind_http_port(docker_apache_t)
-corenet_udp_bind_all_nodes(docker_apache_t)
-corenet_udp_bind_http_port(docker_apache_t)
-sysnet_dns_name_resolve(docker_apache_t)
-'''.split('\n'),'/root/selinux/docker_apache.te')
-		if setenforce: 
-			shutit.add_line_to_file('permissive docker_apache_t','/root/selinux/docker_apache.te')
-		shutit.add_line_to_file('''make -f /usr/share/selinux/devel/Makefile docker_apache.pp
-semodule -i docker_apache.pp
-docker run -d --name selinuxdock --security-opt label:type:docker_apache_t httpd
-'''.split('\n'),'/root/selinux/script.sh')
-		shutit.send('chmod +x /root/selinux/script.sh')
-		# Ensure we have the latest version of docker.
 		shutit.send('wget -qO- https://get.docker.com/builds/Linux/x86_64/docker-latest > docker')
 		shutit.send('mv -f docker /usr/bin/docker')
 		shutit.send('chmod +x /usr/bin/docker')
 		# Recycle docker service.
 		shutit.send('systemctl stop docker')
 		shutit.send('systemctl start docker')
-		# Remove any pre-existing containers.
-		shutit.send('docker rm -f selinuxdock || /bin/true')
-		# Optional code for enforcing>
+		# Insure required software's installed.
+		shutit.send('yum install -y wget selinux-policy-devel')
 		if setenforce: 
 			shutit.send('''sed -i 's/=permissive/=enforcing/' /etc/selinux/config''')
 			# Log out to ensure the prompt stack is stable.
@@ -94,10 +69,37 @@ docker run -d --name selinuxdock --security-opt label:type:docker_apache_t httpd
 			shutit.login(command='vagrant ssh')
 			# Get back to root.
 			shutit.login(command='sudo su')
-		# We should now be root on the virtual box.
-		shutit.send('/root/selinux/script.sh')
-		# Have a look at the log output.
-		shutit.send('sleep 2 && docker logs selinuxdock')
+			# We should now be root on the virtual box.
+			shutit.send('/root/selinux/script.sh')
+			# Have a look at the log output.
+		if compile_policy:
+			shutit.send('mkdir -p /root/selinux')
+			shutit.send('cd /root/selinux')
+			# Ensure we've cleaned up the files we're adding here.
+			shutit.send('rm -rf /root/selinux/docker_apache.tc /root/selinux/script.sh')
+			shutit.add_line_to_file('''policy_module(docker_apache,1.0)
+virt_san	dbox_domain_template(docker_apache)
+allow do	cker_apache_t self: capability { chown dac_override kill setgid setuid net_bind_service sys_chroot sys_nice sys_tty_config } ;
+allow do	cker_apache_t self:tcp_socket create_stream_socket_perms;
+allow do	cker_apache_t self:udp_socket create_socket_perms;
+corenet_	tcp_bind_all_nodes(docker_apache_t)
+corenet_	tcp_bind_http_port(docker_apache_t)
+corenet_	udp_bind_all_nodes(docker_apache_t)
+corenet_	udp_bind_http_port(docker_apache_t)
+sysnet_d	ns_name_resolve(docker_apache_t)
+'''.spli	t('\n'),'/root/selinux/docker_apache.te')
+			if setenforce: 
+				shutit.add_line_to_file('permissive docker_apache_t','/root/selinux/docker_apache.te')
+			shutit.add_line_to_file('''make -f /usr/share/selinux/devel/Makefile docker_apache.pp
+semodule	 -i docker_apache.pp
+docker r	un -d --name selinuxdock --security-opt label:type:docker_apache_t httpd
+'''.spli	t('\n'),'/root/selinux/script.sh')
+			shutit.send('chmod +x /root/selinux/script.sh')
+			# Ensure we have the latest version of docker.
+			# Remove any pre-existing containers.
+			shutit.send('docker rm -f selinuxdock || /bin/true')
+			# Optional code for enforcing>
+			shutit.send('sleep 2 && docker logs selinuxdock')
 		# Un-comment this to get a shell interactively if you want.
 		#shutit.pause_point('Have a shell:')
 		# Log out.
@@ -109,6 +111,7 @@ docker run -d --name selinuxdock --security-opt label:type:docker_apache_t httpd
 	def get_config(self, shutit):
 		shutit.get_config(self.module_id, 'vagrant_dir', '/tmp/vagrant_dir')
 		shutit.get_config(self.module_id, 'setenforce', 'no', boolean=True)
+		shutit.get_config(self.module_id, 'compile_policy', 'yes', boolean=True)
 		return True
 
 
